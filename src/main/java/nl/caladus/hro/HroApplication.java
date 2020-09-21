450048e7ca0f53adbcda6d468f54422209590112
Nathan <nathanernst@hotmail.com>
2020-09-21 16:52:34 +0200

    init

---
+++ src/main/java/nl/caladus/hro/HroApplication.java
@@ -0,0 +1,13 @@
+package nl.caladus.hro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/caladus/hro/HroApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.caladus.hro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
