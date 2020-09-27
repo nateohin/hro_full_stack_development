28824c5f48cb7bce444bc5e83c486bb393548456
Nathan <nathanernst@hotmail.com>
2020-09-27 21:48:17 +0200

    Added spring-boot-actuator

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # hro_full_stack_development
+
+http://fullstack-java-minor.s3-website.eu-central-1.amazonaws.com/
+
 https://spring.io/guides/gs/testing-web/
+
 https://mkyong.com/maven/maven-jacoco-code-coverage-example/
+
 https://mkyong.com/maven/maven-pitest-mutation-testing-example/
+
+https://spring.io/guides/gs/actuator-service/
--- src/test/java/nl/caladus/hro/controller/HomeControllerTest.java
+++ src/test/java/nl/caladus/hro/controller/HomeControllerTest.java
@@ -3,18 +3,29 @@ package nl.caladus.hro.controller;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"management.port=0"})
 class HomeControllerTest {
 
     @LocalServerPort
     private int port;
+
+    @Value("${local.management.port}")
+    private int mgt;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -24,4 +35,14 @@ class HomeControllerTest {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
                 String.class)).contains("Hello, World");
     }
+
+    @Test
+    void shouldReturn200WhenSendingRequestToManagementEndpoint() throws Exception {
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> entity = this.restTemplate.getForEntity(
+                "http://localhost:" + this.mgt + "/actuator/info", Map.class);
+
+        then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
 }
